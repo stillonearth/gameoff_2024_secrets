178581dede1e0e5bfddfc70254f733da1f48904b
Sergei Surovtsev <ssurovsev@gmail.com>
2024-11-11 19:27:10 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@ mod cards;
 
 use bevy::{input::common_conditions::input_toggle_active, prelude::*};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
-use bevy_la_mesa::{events::RenderDeck, DeckArea, HandArea, LaMesaPlugin, LaMesaPluginSettings};
+use bevy_la_mesa::{
+    events::{DrawHand, RenderDeck},
+    DeckArea, HandArea, LaMesaPlugin, LaMesaPluginSettings,
+};
 use bevy_lunex::{prelude::MainUi, UiMinimalPlugins};
-use bevy_novel::{events::EventStartScenario, NovelPlugin, NovelSettings};
+use bevy_novel::{
+    events::{EventHandleNode, EventStartScenario},
+    NovelPlugin, NovelSettings,
+};
 use cards::{Chip, PokerCard};
 use renpy_parser::{
     group_logical_lines,
@@ -30,7 +36,11 @@ fn main() {
             hand_size: 7,
             back_card_path: "cards/card-back2.png".into(),
         })
-        .add_systems(Startup, (setup_camera, start_visual_novel))
+        .add_systems(
+            Startup,
+            (setup_camera, start_visual_novel, setup_card_scene),
+        )
+        .add_systems(Update, handle_new_node)
         .run();
 }
 
@@ -94,7 +104,7 @@ fn start_visual_novel(
 
     println!("asts: {:?}", ast);
 
-    // ew_start_scenario.send(EventStartScenario { ast });
+    ew_start_scenario.send(EventStartScenario { ast });
 
     ew_render_deck.send(RenderDeck::<PokerCard> {
         marker: 1,
@@ -121,10 +131,30 @@ fn setup_camera(mut commands: Commands) {
         Camera3dBundle {
             transform: Transform::from_xyz(0.0, 15.0, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
             camera: Camera {
-                order: 0,
+                order: 2,
                 ..default()
             },
             ..default()
         },
     ));
+}
+
+fn handle_new_node(
+    mut er_handle_node: EventReader<EventHandleNode>,
+    mut ew_draw: EventWriter<DrawHand>,
+) {
+    for event in er_handle_node.read() {
+        match event.ast.clone() {
+            AST::Return(_, _) => {
+                println!("drawing hand");
+
+                ew_draw.send(DrawHand {
+                    deck_marker: 1,
+                    num_cards: 5,
+                    player: 1,
+                });
+            }
+            _ => {}
+        }
+    }
 }
