23abe3c30cee233b754ec9f60a499a32fdc50e0e
Sergei Surovtsev <ssurovsev@gmail.com>
2024-11-11 18:28:40 +0300

    wip; plugins glued

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # gameoff_2024_secrets
 GameOff 2024 Jam Repo
+
+A generative visual novel / card game about a person waking up on derelict space ship.
+
+There is no one around except AI. He tries to stay sane in this depressive environment.
+
+## Requirements
+
+* LLM: bartowski/Phi-3.5-mini-instruct-GGUF
+
+## Card Types
+
+Action Cards
+    Explore (explore a new area of the spaceship)
+    Interact (talk to an NPC or character)
+    Hack (access data or systems on the ship)
+    Defend (protect oneself from threats)
+
+Dialogue Choice Cards
+    Calm (defuse tense situations through diplomacy)
+    Aggressive (take a confrontational approach)
+    Curious (ask questions and gather information)
+    Reveal (share personal secrets or information)
+
+Ability Cards
+    Unlock (gain access to restricted areas or systems)
+    Repair (fix broken equipment or solve technical issues)
+    Hacknet (access the ship's network for hidden data)
+    Persuade (convince someone to help or change their mind)
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,44 @@
-use bevy::prelude::*;
+mod cards;
+
+use bevy::{input::common_conditions::input_toggle_active, prelude::*};
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_la_mesa::{events::RenderDeck, DeckArea, HandArea, LaMesaPlugin, LaMesaPluginSettings};
+use bevy_lunex::{prelude::MainUi, UiMinimalPlugins};
+use bevy_novel::{events::EventStartScenario, NovelPlugin, NovelSettings};
+use cards::{Chip, PokerCard};
+use renpy_parser::{
+    group_logical_lines,
+    lexer::Lexer,
+    list_logical_lines,
+    parsers::{parse_block, AST},
+};
+
+use crate::cards::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
+        .add_plugins((DefaultPlugins, UiMinimalPlugins))
+        .add_plugins(
+            WorldInspectorPlugin::default().run_if(input_toggle_active(false, KeyCode::Escape)),
+        )
+        .add_plugins((NovelPlugin {}, LaMesaPlugin::<PokerCard, Chip>::default()))
+        .insert_resource(NovelSettings {
+            assets_path: "plot".to_string(),
+        })
+        .insert_resource(LaMesaPluginSettings {
+            num_players: 1,
+            hand_size: 7,
+            back_card_path: "cards/card-back2.png".into(),
+        })
+        .add_systems(Startup, (setup_camera, start_visual_novel))
         .run();
 }
 
-/// set up a simple 3D scene
-fn setup(
+fn setup_card_scene(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
 ) {
-    // circular base
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Circle::new(4.0)),
-        material: materials.add(Color::WHITE),
-        transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
-        ..default()
-    });
-    // cube
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
-        material: materials.add(Color::srgb_u8(124, 144, 255)),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
     // light
     commands.spawn(PointLightBundle {
         point_light: PointLight {
@@ -36,9 +48,83 @@ fn setup(
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..default()
     });
-    // camera
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
+
+    // Deck
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Plane3d::default().mesh().size(2.5, 3.5).subdivisions(10)),
+            material: materials.add(Color::BLACK),
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0))
+                .with_rotation(Quat::from_rotation_y(std::f32::consts::PI / 2.0)),
+            visibility: Visibility::Hidden,
+            ..default()
+        },
+        DeckArea { marker: 1 },
+        Name::new("Deck 1 -- Play Cards"),
+    ));
+
+    // Hand
+    commands.spawn((
+        Name::new("HandArea - Player 1"),
+        TransformBundle {
+            local: Transform::from_translation(Vec3::new(0.0, 1.5, 5.8))
+                .with_rotation(Quat::from_rotation_x(std::f32::consts::PI / 4.0)),
+            ..default()
+        },
+        HandArea { player: 1 },
+    ));
+}
+
+fn load_scenario(path: String) -> Vec<AST> {
+    let lines = list_logical_lines(&path).unwrap();
+    let blocks = group_logical_lines(lines).unwrap();
+
+    let l = &mut Lexer::new(blocks.clone(), true);
+
+    let (ast, _) = parse_block(l);
+    ast
+}
+
+fn start_visual_novel(
+    mut ew_start_scenario: EventWriter<EventStartScenario>,
+    mut ew_render_deck: EventWriter<RenderDeck<PokerCard>>,
+) {
+    let path = "assets/plot/intro.rpy";
+    let ast = load_scenario(path.to_string());
+
+    println!("asts: {:?}", ast);
+
+    // ew_start_scenario.send(EventStartScenario { ast });
+
+    ew_render_deck.send(RenderDeck::<PokerCard> {
+        marker: 1,
+        deck: load_poker_deck(),
     });
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn((
+        Name::new("Camera 2d"),
+        MainUi,
+        Camera2dBundle {
+            transform: Transform::from_xyz(0.0, 0.0, 1000.0),
+            camera: Camera {
+                order: 1,
+                ..default()
+            },
+            ..default()
+        },
+    ));
+
+    commands.spawn((
+        Name::new("Camera 3d"),
+        Camera3dBundle {
+            transform: Transform::from_xyz(0.0, 15.0, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
+            camera: Camera {
+                order: 0,
+                ..default()
+            },
+            ..default()
+        },
+    ));
 }
